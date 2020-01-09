ff6b68b5ec897ed9733d5a08439c18ea9885a4dc
CloudBytes101 <45950137+CloudBytes101@users.noreply.github.com>
2020-01-09 15:12:07 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "Cat-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["US-EAST-1"]
+  azs             = ["us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
